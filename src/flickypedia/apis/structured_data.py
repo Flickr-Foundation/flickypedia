a25d1f045f08d21a483ee696daad0cb1b2bd3608
Alex Chan <alex@alexwlchan.net>
2023-10-10 17:24:22 +0100

    Add a structured data entity for 'Copyright status: Copyrighted'

--- src/flickypedia/apis/structured_data.py
+++ src/flickypedia/apis/structured_data.py
@@ -6,7 +6,25 @@ The goal of this file is to create some helpers and templates to reduce
 the amount of repetition required when creating these entities.
 """
 
-from flickypedia.apis.wikidata import lookup_flickr_user_in_wikidata, WikidataProperties
+from flickypedia.apis.wikidata import (
+    lookup_flickr_user_in_wikidata,
+    WikidataEntities,
+    WikidataProperties,
+)
+
+
+def _link_to_wikibase_entity(*, property, wikidata_id):
+    return {
+        "mainsnak": {
+            "snaktype": "value",
+            "property": property,
+            "datavalue": {
+                "type": "wikibase-entityid",
+                "value": {"id": wikidata_id},
+            },
+        },
+        "type": "statement",
+    }
 
 
 def create_flickr_creator_data(user_id, username, realname):
@@ -22,17 +40,9 @@ def create_flickr_creator_data(user_id, username, realname):
     wikidata_id = lookup_flickr_user_in_wikidata(id=user_id, username=username)
 
     if wikidata_id is not None:
-        return {
-            "mainsnak": {
-                "snaktype": "value",
-                "property": WikidataProperties.CREATOR,
-                "datavalue": {
-                    "type": "wikibase-entityid",
-                    "value": {"id": wikidata_id},
-                },
-            },
-            "type": "statement",
-        }
+        return _link_to_wikibase_entity(
+            property=WikidataProperties.CREATOR, wikidata_id=wikidata_id
+        )
     else:
         qualifier_values = [
             (WikidataProperties.AUTHOR_NAME, realname or username),
@@ -64,3 +74,21 @@ def create_flickr_creator_data(user_id, username, realname):
             ],
             "type": "statement",
         }
+
+
+def create_copyright_status_data(status):
+    """
+    Create a structured data claim for a copyright status.
+
+    Currently this only supports "Copyright status: copyrighted", but
+    it might evolve in future if we e.g. support "no known copyright status".
+    """
+    if status != "copyrighted":
+        raise ValueError(
+            f"Unable to map a copyright status which isn't “copyrighted”: {status!r}"
+        )
+
+    return _link_to_wikibase_entity(
+        property=WikidataProperties.COPYRIGHT_STATUS,
+        wikidata_id=WikidataEntities.Copyrighted,
+    )
--- tests/apis/test_structured_data.py
+++ tests/apis/test_structured_data.py
@@ -2,7 +2,10 @@ import json
 
 import pytest
 
-from flickypedia.apis.structured_data import create_flickr_creator_data
+from flickypedia.apis.structured_data import (
+    create_copyright_status_data,
+    create_flickr_creator_data,
+)
 
 
 @pytest.mark.parametrize(
@@ -28,6 +31,24 @@ from flickypedia.apis.structured_data import create_flickr_creator_data
 )
 def test_create_flickr_creator_data(vcr_cassette, kwargs, filename):
     result = create_flickr_creator_data(**kwargs)
+    expected = json.load(open(f"tests/fixtures/structured_data/{filename}"))
+
+    assert result == expected
+
+
+def test_create_copyright_status_data_fails_for_unknown_value():
+    with pytest.raises(ValueError, match="Unable to map a copyright status"):
+        create_copyright_status_data(status="No known copyright status")
+
+
+@pytest.mark.parametrize(
+    ["kwargs", "filename"],
+    [
+        ({"status": "copyrighted"}, "copyright_status_copyrighted.json"),
+    ],
+)
+def test_create_copyright_status_data(kwargs, filename):
+    result = create_copyright_status_data(**kwargs)
     expected = json.load(open(f"tests/fixtures/structured_data/{filename}"))
 
     assert result == expected
