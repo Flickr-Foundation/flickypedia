b443f0cd66a75183188e4e5e0484b48124b54578
Alex Chan <alex@alexwlchan.net>
2023-10-19 12:08:20 +0100

    Start testing the OAuth 2 authorize flow

---
+++ tests/test_auth.py
@@ -0,0 +1,8 @@
+def test_oauth2_authorize_wikimedia_redirects_you(app):
+    app.config["OAUTH2_PROVIDERS"]["wikimedia"]["client_id"] = "example1234"
+
+    with app.test_client() as client:
+        resp = client.get("/authorize/wikimedia")
+
+    assert resp.status_code == 302
+    assert resp.headers['location'] == 'https://meta.wikimedia.org/w/rest.php/oauth2/authorize?client_id=example1234&response_type=code'
