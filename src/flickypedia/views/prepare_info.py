dc0b19d5958689aff63ea55250997f78cf709d75
Alex Chan <alex@alexwlchan.net>
2023-10-26 10:57:53 +0100

    Plumb in the form properly

--- src/flickypedia/views/prepare_info.py
+++ src/flickypedia/views/prepare_info.py
@@ -13,10 +13,102 @@ This page gets two arguments as query parameters:
 
 """
 
-from flask import render_template
+from flask import abort, current_app, flash, render_template, request
+from flask_wtf import FlaskForm, Form
 from flask_login import login_required
+from wtforms import FormField, HiddenField, SelectField, StringField, SubmitField
+from wtforms.validators import DataRequired
+
+from .select_photos import get_cached_api_response
+
+
+def create_prepare_info_form(photos):
+    """
+    Create a Flask form with a PhotoInfoForm (list of fields) for each
+    photo in the list.  This allows us to render a form like:
+
+        - photo 1: title ____ / caption ____ / categories ____
+        - photo 2: title ____ / caption ____ / categories ____
+        - photo 3: title ____ / caption ____ / categories ____
+
+    even though the list of photos is created dynamically from
+    the Flickr API.
+
+    The created fields will have be boolean fields with the ID 'photo_{id}',
+    similar to if we'd written:
+
+        class PrepareInfoForm(FlaskForm):
+            cached_api_response_id = …
+            submit = …
+            photo_1 = FormField()
+            photo_2 = FormField()
+            photo_3 = FormField()
+
+    The labels on each of the fields will be a dict with all the photo data,
+    which can be used to render nice previews/labels.
+    """
+
+    class PhotoInfoForm(Form):
+        title = StringField(validators=[DataRequired()])
+        short_caption = StringField(validators=[DataRequired()])
+        categories = StringField()
+
+    class CustomForm(FlaskForm):
+        cached_api_response_id = HiddenField("cached_api_response_id")
+        submit = SubmitField("Prepare info")
+
+        # TODO: Get a proper list of languages here
+        language = SelectField(
+            "language", choices=[("en", "English"), ("de", "Deutsch")]
+        )
+
+    for p in photos:
+        setattr(CustomForm, f"photo_{p['id']}", FormField(PhotoInfoForm, label=p))
+
+    return CustomForm()
 
 
 @login_required
 def prepare_info():
-    return render_template("prepare_info.html", current_step="prepare_info")
+    try:
+        selected_photo_ids = set(request.args["selected_photo_ids"].split(","))
+        cached_api_response_id = request.args["cached_api_response_id"]
+    except KeyError:
+        abort(400)
+
+    # Look up the cached Flickr API response from the previous stop.
+    # We can then look up the list of photo from the list of selected
+    # photo IDs.
+    #
+    # This will have the same order as the photos in the previous page,
+    # so doing this filter will show the photos in the same order in
+    # this page.
+    api_response = get_cached_api_response(cached_api_response_id)
+
+    selected_photos = [
+        photo for photo in api_response["photos"] if photo["id"] in selected_photo_ids
+    ]
+
+    # Do some basic consistency checks.  These assertions could fail
+    # if somebody has mucked around with the URL query parameters,
+    #  but in that case I'm okay to let it fail.
+    assert len(selected_photos) == len(selected_photo_ids)
+    assert all(
+        photo["license"]["id"] in current_app.config["ALLOWED_LICENSES"]
+        for photo in selected_photos
+    )
+
+    # Now construct the "prepare info" form.
+    prepare_info_form = create_prepare_info_form(photos=selected_photos)
+
+    if prepare_info_form.validate_on_submit():
+        from pprint import pprint
+
+        pprint(form.data)
+        flash("Ready to upload!")
+
+    return render_template(
+        "prepare_info.html",
+        current_step="prepare_info",
+        prepare_info_form=prepare_info_form,
+    )
