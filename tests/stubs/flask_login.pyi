b53e1ce8e725ea67b223bd2b4ffdf0fed4815ad8
Alex Chan <alex@alexwlchan.net>
2023-11-07 15:27:31 +0000

    Add the stubs directory

--- tests/stubs/flask_login.pyi
+++ tests/stubs/flask_login.pyi
@@ -2,8 +2,6 @@ from typing import Any, Callable
 
 from flickypedia.auth import WikimediaUserSession
 
-
 current_user: WikimediaUserSession
-
 
 def login_required(func: Callable[..., Any]) -> Callable[..., Any]: ...
