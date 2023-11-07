244c59589b95435b633110a301ac4a6f284754a1
Alex Chan <alex@alexwlchan.net>
2023-11-07 21:45:34 +0000

    Add a few more types

--- src/flickypedia/views/wait_for_upload.py
+++ src/flickypedia/views/wait_for_upload.py
@@ -2,17 +2,18 @@ from flask import jsonify, render_template
 from flask_login import login_required
 
 from flickypedia.tasks import get_status
+from ._types import ViewResponse
 
 
 @login_required
-def wait_for_upload(task_id):
+def wait_for_upload(task_id: str) -> ViewResponse:
     return render_template(
         "wait_for_upload.html", task_id=task_id, current_step="upload_to_wikimedia"
     )
 
 
 @login_required
-def get_upload_status(task_id):
+def get_upload_status(task_id: str) -> ViewResponse:
     status = get_status(task_id)
 
     return jsonify(status)
