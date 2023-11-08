e55ea1cbd6d5421c8e96c5fed244f55665b2905b
Alex Chan <alex@alexwlchan.net>
2023-11-08 14:37:50 +0000

    Add a comment explaining how the page works

--- src/flickypedia/views/wait_for_upload.py
+++ src/flickypedia/views/wait_for_upload.py
@@ -1,3 +1,49 @@
+"""
+A page where a user can track the progress of their upload.
+
+We show them a grid of photos, and the grid gradually updates showing
+them the progress.  A photo starts as "not started", then becomes
+"in progress", and finally "done" (succeeded) or "not done" (failed).
+
+    +-------------+   +--------------+   +--------------+
+    |  ──────▄▀─  |   | ♫░░█▄        |   |   ─▄▀─▄▀     |
+    |  ─█▀▀▀█▀█─  |   | ░♫░████▄▄▄▄  |   |   ──▀──▀     |
+    |  ──▀▄░▄▀──  |   | ♫░░██▀▀░░░░█ |   |   █▀▀▀▀▀█▄   |
+    |  ────█────  |   | ░♫░▀░░▓▓▓▓▀  |   |   █░░░░░█─█  |
+    |  ──▄▄█▄▄──  |   | ░░░░▄██████▄ |   |   ▀▄▄▄▄▄▀▀   |
+    +-------------+   +--------------+   +--------------+
+         done!              done!           not done :(
+
+    +-------------+   +--------------+   +--------------+
+    | ─────────▄  |   | ─▄▄██████▄▄─ |   |░░░░░░░░░░░▄██|
+    | ───────▄██  |   | ██▀▄█▄▄█▄▀██ |   |░░░░░░░░░▄████|
+    | ─▄▀██▀█▀█▀█▀|   | ▀▀▄██▀▀██▄▀▀ |   |░░░░░░░▄██████|
+    | ▀▀▀▀▀███▀▀▀ |   | ▄███─██─███▄ |   |░▄██▄▄███▀░██░|
+    | ──────▀█    |   | █████▄▄█████ |   |████████▀░░█░░|
+    +-------------+   +--------------+   +--------------+
+         done!           in progress        not started
+
+The cool ASCII art comes from https://fsymbols.com/text-art/twitter/
+
+== How it works ==
+
+When we render the initial page, we get the current state of the task
+and render the grid of photos.  This is a <ul> with an <li> for each
+of the photos, and each <li> records the current state in an attribute.
+
+That state attribute defines how the photo gets styled in CSS.
+
+The page gets refreshed periodically:
+
+*   For JS users, there's a JavaScript function on the page that polls
+    an API endpoint for retrieving task status.
+*   For non-JS users, we use a <meta refresh> tag to force a page
+    reload on a schedule.
+
+When the upload completes, we redirect the user to the next page.
+
+"""
+
 from flask import jsonify, render_template
 from flask_login import login_required
 
