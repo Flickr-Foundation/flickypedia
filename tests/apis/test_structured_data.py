fc6b3586b160d9079aa2e3277b1ee42eaeaad98f
Alex Chan <alex@alexwlchan.net>
2023-11-09 11:37:55 +0000

    Fix the copyright status for non-copyright photos

--- tests/apis/test_structured_data.py
+++ tests/apis/test_structured_data.py
@@ -70,19 +70,23 @@ def test_create_flickr_creator_statement(
     assert result == expected
 
 
-def test_create_copyright_status_statement_fails_for_unknown_value() -> None:
+def test_create_copyright_status_statement_fails_for_unknown_license() -> None:
     with pytest.raises(ValueError, match="Unable to map a copyright status"):
-        create_copyright_status_statement(status="No known copyright status")
+        create_copyright_status_statement(license_id="cc-by-nc-2.0")
 
 
 @pytest.mark.parametrize(
-    ["status", "filename"],
+    ["license_id", "filename"],
     [
-        ("copyrighted", "copyright_status_copyrighted.json"),
+        ("cc-by-2.0", "copyright_status_copyrighted.json"),
+        ("cc-by-sa-2.0", "copyright_status_copyrighted.json"),
+        ("usgov", "copyright_status_usgov.json"),
+        ("cc0-1.0", "copyright_status_public_domain.json"),
+        ("pdm", "copyright_status_public_domain.json"),
     ],
 )
-def test_create_copyright_status_statement(status: str, filename: str) -> None:
-    result = create_copyright_status_statement(status=status)
+def test_create_copyright_status_statement(license_id: str, filename: str) -> None:
+    result = create_copyright_status_statement(license_id=license_id)
     expected = get_fixture(filename)
 
     assert result == expected
