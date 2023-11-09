1a2d916bc87004a2e4c1a3994f09cb2d7d5b9e4e
Alex Chan <alex@alexwlchan.net>
2023-11-09 08:56:01 +0000

    Start building the "upload completed" screen

--- src/flickypedia/views/upload_complete.py
+++ src/flickypedia/views/upload_complete.py
@@ -1,8 +1,19 @@
+from flask import render_template
 from flask_login import login_required
 
+from flickypedia.tasks import get_status
 from ._types import ViewResponse
 
 
 @login_required
 def upload_complete(task_id: str) -> ViewResponse:
-    return "wow you’re done!"
+    status = get_status(task_id)
+
+    assert status['ready']
+
+    assert all(
+        item['status'] in {'succeeded', 'failed'}
+        for item in status['progress']
+    )
+
+    return render_template("upload_complete.html", status=status, current_step="upload_to_wikimedia")
