a66660021ae264b22da6d5860271951e085848a8
Alex Chan <alex@alexwlchan.net>
2023-11-23 17:18:29 +0000

    Wire the new command into the CLI

--- src/flickypedia/cli.py
+++ src/flickypedia/cli.py
@@ -45,3 +45,18 @@ def run_background_worker() -> None:
     with app.app_context():
         q = uploads_queue()
         q.process_tasks()
+
+
+@main.group(
+    short_help="Commands for the web app for copying photos from Flickr to Wikimedia",
+    help="Commands for the web app for copying openly-licensed photos from Flickr to Wikimedia Commons.",
+)
+def uploadr() -> None:
+    pass
+
+
+@uploadr.command(short_help="Store an OAuth token for the Flickypedia Bot user.")
+def store_flickypedia_bot_token() -> None:
+    from flickypedia.uploadr.auth import store_flickypedia_user_oauth_token
+
+    store_flickypedia_user_oauth_token()
