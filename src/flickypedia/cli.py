72fafd48dfcea5e58f6a52c49412b04d0e1e9587
Alex Chan <alex@alexwlchan.net>
2023-11-24 09:49:16 +0000

    Start to break up the monolithic cli.py file

--- src/flickypedia/cli.py
+++ src/flickypedia/cli.py
@@ -3,6 +3,8 @@ import sys
 
 import click
 
+from flickypedia.uploadr import uploadr_cli
+
 
 @click.group()
 def main() -> None:  # pragma: no cover
@@ -47,16 +49,4 @@ def run_background_worker() -> None:
         q.process_tasks()
 
 
-@main.group(
-    short_help="Commands for the web app for copying photos from Flickr to Wikimedia",
-    help="Commands for the web app for copying openly-licensed photos from Flickr to Wikimedia Commons.",
-)
-def uploadr() -> None:
-    pass
-
-
-@uploadr.command(short_help="Store an OAuth token for the Flickypedia Bot user.")
-def store_flickypedia_bot_token() -> None:
-    from flickypedia.uploadr.auth import store_flickypedia_user_oauth_token
-
-    store_flickypedia_user_oauth_token()
+main.add_command(uploadr_cli)
