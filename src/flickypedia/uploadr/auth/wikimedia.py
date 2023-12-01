fea9f3d5e409b94902d306885c6d9ef4cf4bd728
Alex Chan <alex@alexwlchan.net>
2023-12-01 13:47:55 +0000

    try another approach to logging

--- src/flickypedia/uploadr/auth/wikimedia.py
+++ src/flickypedia/uploadr/auth/wikimedia.py
@@ -414,7 +414,7 @@ def oauth2_callback_wikimedia() -> ViewResponse:
             state=state,
         )
     except Exception as exc:
-        logging.warning(f"Error exchanging authorization code for token: {exc}")
+        current_app.logger.error(f"Error exchanging authorization code for token: {exc}")
         abort(403)
 
     # Get info about the logged in user
