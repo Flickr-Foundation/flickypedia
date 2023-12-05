a2d9fc99810c10c0700d25a8a5896fd43db43f61
Alex Chan <alex@alexwlchan.net>
2023-12-05 11:01:32 +0000

    Pull out a handy "get completed task" utility function

--- src/flickypedia/uploadr/views/say_thanks.py
+++ src/flickypedia/uploadr/views/say_thanks.py
@@ -1,19 +1,12 @@
 from flask import render_template
 from flask_login import login_required
 
-from ..uploads import uploads_queue
+from .upload_complete import get_completed_task
 from ._types import ViewResponse
 
 
 @login_required
 def say_thanks(task_id: str) -> ViewResponse:
-    q = uploads_queue()
-    task = q.read_task(task_id)
-
-    assert task["state"] == "completed"
-    assert all(
-        item["state"] in {"succeeded", "failed"}
-        for item in task["task_output"].values()
-    )
+    get_completed_task(task_id)
 
     return render_template("say_thanks.html", current_step="say_thanks")
