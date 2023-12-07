2b34348cd8506bfa1870b71b15dc503fdac642d0
Alex Chan <alex@alexwlchan.net>
2023-12-07 17:08:08 +0000

    Add caching to validate_typeddict
    In informal testing, this results in an ~75× speed-up when paging through
    an SDC snapshot.

--- src/flickypedia/types/__init__.py
+++ src/flickypedia/types/__init__.py
@@ -1,4 +1,5 @@
-from typing import Any, TypeVar
+import functools
+from typing import Any, Hashable, TypeVar
 
 from pydantic import ConfigDict, TypeAdapter
 
@@ -6,20 +7,38 @@ from pydantic import ConfigDict, TypeAdapter
 T = TypeVar("T")
 
 
-def validate_typeddict(t: Any, model: type[T]) -> T:
+@functools.cache
+def get_validator(model: type[T]) -> TypeAdapter[T]:
     """
-    Check that some data matches a TypedDict.
-
-    We use this to check that the structured data we receive
-    from Wikimedia matches our definitions, so we can use it
-    in type-checked Python.
-
-    See https://stackoverflow.com/a/77386216/1558022
+    Get the validator for a given type.  This is a moderately expensive
+    process, so we cache the result -- we only need to create the
+    validator once for each type.
     """
     try:
         model.__pydantic_config__ = ConfigDict(extra="forbid")  # type: ignore
     except AttributeError:
         pass
 
-    TypedDictValidator = TypeAdapter(model)
-    return TypedDictValidator.validate_python(t, strict=True)
+    return TypeAdapter(model)
+
+
+def validate_typeddict(t: Any, model: type[T]) -> T:
+    """
+    Check that some data matches a TypedDict.
+
+    We use this to check that the structured data we receive from
+    Wikimedia matches our definitions, so we can use the data in our
+    type-checked Python.
+
+    See https://stackoverflow.com/a/77386216/1558022
+    """
+    # This is to fix an issue from the type checker:
+    #
+    #     Argument 1 to "__call__" of "_lru_cache_wrapper"
+    #     has incompatible type "type[T]"; expected "Hashable"
+    #
+    assert isinstance(model, Hashable)
+
+    validator = get_validator(model)
+
+    return validator.validate_python(t, strict=True)
