3230d20f691b5716791e8e3a4c93666a2dd859b7
Alex Chan <alex@alexwlchan.net>
2023-12-08 11:51:37 +0000

    Start writing the CSV file

--- src/flickypedia/extractr/cli.py
+++ src/flickypedia/extractr/cli.py
@@ -1,3 +1,6 @@
+import csv
+import re
+
 import click
 import tqdm
 
@@ -15,17 +18,35 @@ def extractr() -> None:
 @extractr.command(help="Get a list of Flickr photos on Commons.")
 @click.argument("SNAPSHOT_PATH")
 def get_list_of_photos(snapshot_path: str) -> None:
-    for entry in tqdm.tqdm(parse_sdc_snapshot(snapshot_path)):
-        try:
-            flickr_photo_id = find_flickr_photo_id(entry['statements'])
-        except Exception:
-            import json
-            from pprint import pprint; print(json.dumps(entry['statements']))
-            raise
+    # The name of the snapshot is something like:
+    #
+    #     commons-20231009-mediainfo.json.bz2
+    #
+    # If we can, extract the data and use it to label the spreadsheet we
+    # generate
+    date_match = re.search(r"\-(\d+)\-", snapshot_path)
+    if date_match is None:
+        csv_path = "flickr_ids_from_sdc.csv"
+    else:
+        csv_path = f"flickr_ids_from_sdc.{date_match.group(1)}.csv"
 
+    with open(csv_path, "w") as out_file:
+        writer = csv.DictWriter(out_file, fieldnames=["flickr_photo_id", "wikimedia_page_id", "wikimedia_page_title"])
+        writer.writeheader()
 
-        from pprint import pprint; pprint()
-        from pprint import pprint; pprint(entry['statements'])
-        break
+        for entry in tqdm.tqdm(parse_sdc_snapshot(snapshot_path)):
+            try:
+                flickr_photo_id = find_flickr_photo_id(entry['statements'])
+            except Exception:
+                import json
+                from pprint import pprint; print(json.dumps(entry['statements']))
+                raise
 
-    print(snapshot_path)
+            if flickr_photo_id is not None:
+                writer.writerow({
+                    "flickr_photo_id": flickr_photo_id,
+                    "wikimedia_page_id": entry['id'],
+                    'wikimedia_page_title': entry['title']
+                })
+
+    print(csv_path)
