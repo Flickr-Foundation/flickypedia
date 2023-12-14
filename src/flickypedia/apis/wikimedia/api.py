c50c8d8e4d5c1563480b30831ab9d7233c2b789c
Alex Chan <alex@alexwlchan.net>
2023-12-14 16:41:17 +0000

    remvoe more debugging code

--- src/flickypedia/apis/wikimedia/api.py
+++ src/flickypedia/apis/wikimedia/api.py
@@ -276,8 +276,6 @@ class WikimediaApi:
         if "missing" in page:
             raise MissingFileException(filename)
 
-        print(resp["entities"].keys())
-
         statements = list(resp["entities"].values())[0]["statements"]
 
         if statements == []:
