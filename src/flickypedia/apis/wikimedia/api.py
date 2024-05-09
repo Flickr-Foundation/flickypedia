bfcef983cc052423c184354114c8b9c7556017b5
Alex Chan <alex@alexwlchan.net>
2024-05-09 08:21:28 +0100

    Introduce the notion of a 'base API'

--- src/flickypedia/apis/wikimedia/api.py
+++ src/flickypedia/apis/wikimedia/api.py
@@ -13,8 +13,6 @@ import re
 import typing
 from xml.etree import ElementTree as ET
 
-import httpx
-
 from flickypedia.types import validate_typeddict
 from flickypedia.types.structured_data import ExistingClaims, NewClaims
 from flickypedia.types.wikimedia import UserInfo, ShortCaption, TitleValidation
@@ -22,75 +20,21 @@ from flickypedia.utils import find_required_elem
 from .exceptions import (
     WikimediaApiException,
     UnknownWikimediaApiException,
-    InvalidAccessTokenException,
     DuplicateFilenameUploadException,
     DuplicatePhotoUploadException,
     MissingFileException,
 )
+from .base import HttpxImplementation, WikimediaApiBase
 from .languages import LanguageMatch, order_language_list
 
 
-class WikimediaApi:
-    def __init__(self, client: httpx.Client) -> None:
-        self.client = client
-
-    def _request(
-        self,
-        *,
-        method: str,
-        params: dict[str, str] | None = None,
-        data: dict[str, str] | None = None,
-        timeout: int | None = None,
-    ) -> typing.Any:
-        resp = self.client.request(
-            method,
-            url="https://commons.wikimedia.org/w/api.php",
-            params=params,
-            data=data,
-            timeout=timeout,
-        )
-
-        # When something goes wrong, we get an ``error`` key in the response.
-        #
-        # Detect this here and throw an exception, so callers can assume
-        # there was no issue if this returns cleanly.
-        #
-        # See https://www.mediawiki.org/wiki/Wikibase/API#Response
-        try:
-            error = resp.json()["error"]
-
-            if error["code"] == "mwoauth-invalid-authorization":
-                raise InvalidAccessTokenException(error["info"])
-            else:
-                raise UnknownWikimediaApiException(resp)
-        except KeyError:
-            pass
-
-        return resp.json()
-
+class WikimediaApi(HttpxImplementation, WikimediaApiBase):
     def _get(self, params: dict[str, str]) -> typing.Any:
+        return self._get_json(params=params)
         return self._request(method="GET", params={**params, "format": "json"})
 
     def _post(self, data: dict[str, str], timeout: int | None = None) -> typing.Any:
-        return self._request(
-            method="POST",
-            data={**data, "format": "json", "token": self.get_csrf_token()},
-            timeout=timeout,
-        )
-
-    def get_csrf_token(self) -> str:
-        """
-        Get a CSRF token from the Wikimedia API.
-
-        This is required for certain API actions that modify data in
-        Wikimedia.  External callers are never expected to use this,
-        but functions from this class will call it when they need a token.
-
-        See https://www.mediawiki.org/wiki/API:Tokens
-        """
-        resp = self._get(params={"action": "query", "meta": "tokens", "type": "csrf"})
-
-        return resp["query"]["tokens"]["csrftoken"]  # type: ignore
+        return self._post_json(data=data, timeout=timeout)
 
     def get_userinfo(self) -> UserInfo:
         """
