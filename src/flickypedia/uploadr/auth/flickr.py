9e97fa60373415ffd30ca14de891f8b35c5e250a
Alex Chan <alex@alexwlchan.net>
2025-06-30 15:44:54 +0100

    Just inline the `get_required_password` function

--- src/flickypedia/uploadr/auth/flickr.py
+++ src/flickypedia/uploadr/auth/flickr.py
@@ -7,7 +7,6 @@ import click
 from flask import abort, current_app, redirect, request, session, url_for
 from flask_login import current_user, login_required
 import keyring
-from nitrate.passwords import get_required_password
 from nitrate.xml import find_required_elem, find_required_text
 import werkzeug
 
@@ -133,6 +132,33 @@ def get_flickypedia_bot_oauth_client() -> OAuth1Client:
     )
 
     return client
+
+
+def get_required_password(service_name: str, username: str) -> str:
+    """
+    Retrieve a password from the keychain, or throw if it's missing.
+    """
+    # We wrap this API because keyring will return ``None`` rather
+    # than tell you a password is missing, e.g.
+    #
+    #     >>> import keyring
+    #     >>> pw = keyring.get_password("doesnotexist", "doesnotexist")
+    #     >>> print(pw)
+    #     None
+    #
+    # It's better to throw an error early than let this empty value
+    # propagate into our code and bubble up elsewhere.
+    #
+    # e.g. if we're calling the Flickr API using an API key retrieved
+    # using keyring, we'd rather know immediately that it's empty than
+    # get a cryptic "invalid API key" error from the Flickr API.
+    #
+    password = keyring.get_password(service_name, username)
+
+    if password is None:
+        raise RuntimeError(f"Could not retrieve password {(service_name, username)}")
+
+    return password
 
 
 @login_required
